e636bdf007806836942b5b09ea70261c4a491b75
harmony <harmony.dgt@gmail.com>
2023-11-03 19:41:20 -0300

    '..'

---
+++ assets/js/app.js
@@ -0,0 +1,46 @@
+const app = document.getElementById('app')
+
+app.innerHTML=`
+
+    <div class='hearder'>
+        <h1>
+            Flex Delivery
+        </h1>
+    </div>
+
+
+    <div >
+
+        <div>
+            <h3>Aplicativos</h3>
+        </div>
+
+
+
+        <div class='appGalery'>
+
+            <a targt='_blank' href='https://harmonydigital.github.io/xarapp/'>
+                <img src='assets/img/bebidas.png'>
+                <span>Bebidas Delivery</span>
+            </a>
+
+            <a href='#'>
+                <img src='assets/img/pizzas.png'>
+                <span>Pizzas Delivery</span>
+            </a>
+
+            <a href='#'>
+                <img src='assets/img/camarao.png'>
+                <span>Praia Delivery</span>
+            </a>
+        </div>
+
+    </div>
+
+    <footer>
+        <a href='https://www.instagram.com/harmonydigitalflow/' barget='_blank'>
+            @harmonydigitalflow
+        </a>
+    </footer>
+
+` 
